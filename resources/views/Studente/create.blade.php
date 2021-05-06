25b0d5f11c21dfdfdcdac430453924603c324971
ETTARAK <ettarak.zouhair@gmail.com>
2021-05-06 12:41:02 +0000

    Create layout and file blade Student for Create method

---
+++ resources/views/Studente/create.blade.php
@@ -0,0 +1,15 @@
+@extends('shared.layout')
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Student</div>
+            <div class="panel-body">
+
+            </div>
+          </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/shared/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <title>Laravel</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12" style="margin-top: 10px !important;">
+                <div class="jumbotron">
+                    <h1 class="text-center">Laravel 8 CRUD</h1>
+
+                  </div>
+            </div>
+
+                @yield('content')
+
+        </div>
+    </div>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
